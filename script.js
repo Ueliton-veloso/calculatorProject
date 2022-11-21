8a8f9ef18bcd86190c82cae41e198c1fabf20f95
Ueliton Veloso <wellingtontom87.tt@gmail.com>
2022-11-21 13:41:12 -0300

    update

--- script.js
+++ script.js
@@ -3,55 +3,38 @@ const calculator = document.querySelector(".calculator").addEventListener("click
 function buttonEventInteraction(e) {
     if(e.target.type == 'button'){
         result(e.target.value)
-        deleteAc(e.target.value)
-
-        if (e.target.value != "ac" && e.target.value != "=") {
+        allClear(e.target.value)
+        if(e.target.value != "ac" && e.target.value != "=") {
            let displayCharacters = document.querySelector(".calculator > div > h1").innerHTML += e.target.value
-           maxCharacters(displayCharacters, 50)
-           
-        }
-        
+           maxCharacters(displayCharacters, 35)  
+        }  
     }
-
 }
-
 
 const result = (buttonResult) => {
     let totalResult = document.querySelector(".calculator > div > h1").innerHTML
-    if (totalResult){
+    if(totalResult){
         if(buttonResult == "="){
-
             document.querySelector(".calculator > div > h1").style.flexDirection = "column-reverse";
             let showResult = document.querySelector(".calculator > div > h1").innerHTML = eval(totalResult)
-
-          
         } else{
             document.querySelector(".calculator > div > h1").style.flexDirection = "row-reverse";
         }
-    } 
-    
+    }  
 }
 
-
-
-const deleteAc = (del) => {
+const allClear = (displayItem) => {
     let totalResult = document.querySelector(".calculator > div > h1").innerHTML
-    if (totalResult){
-        if(del == "ac"){
+    if(totalResult){
+        if(displayItem == "ac"){
             document.querySelector(".calculator > div > h1").innerHTML = ""
         }
     } 
-    
 }
 
-
-
-
-
-const maxCharacters = (characters, pause) => {
-    if (characters.length > pause){
+const maxCharacters = (characters, limit) => {
+    if(characters.length > limit){
         document.querySelector(".calculator > div > h1").innerHTML = ""
-
     }
 }
 
